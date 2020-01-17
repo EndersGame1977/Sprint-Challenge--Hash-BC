d6ad2aad4cba0b4418cb8e483bc959570e19d63b
Jason Prince <jasoncprince@gmail.com>
2020-01-17 09:39:51 -0700

    Both hashtables are passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,31 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    #   Need at least two weights to compare
+    if length <= 1:
+        return None
+
+    #   The weight amount needs to be set as a key and the index of it as the value in a hashtable.
+    #   The hash_table_insert function calls for a hashtable, key, and value.
+    #   Loop over the weights list to place each weight as a key and corosponding index as a value to that key in a hashtable.
+    for weight in range(length):
+        hash_table_insert(ht, weights[weight], weight)
+
+    #   Looking for the weight in the hashtable that when added to the weight in the weight list would equal the limit.
+    #   Loop over the weight list and subtract that weight from the limit to get the weight needed that would sum to the limit.
+    #   Pass that desired weight to the hash_table_retrieve function
+    #   If the desired weight is in hashtable. Return that weigth and the current weight.
+    for current_weight in range(length):
+        desired_weight = limit - weights[current_weight]
+        if hash_table_retrieve(ht, desired_weight) is not None:
+            desired_weight = hash_table_retrieve(ht, desired_weight)
+            return(desired_weight, current_weight)
 
     return None
 
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,18 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    #   Insert source and destination from tickets into hashtable
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    #   Retrieve ticket from hashtable that has the key of "NONE"
+    #   Set that ticket destination value to the first position in the route.
+    route[0] = hash_table_retrieve(hashtable, "NONE")
+
+    #   Start at index 1 of route.
+    #   Pass the destination of the previous ticket as the source of the next ticket in the hash_table_retrieve function
+    #   Assign the retrieved destination value to the route list.
+    for i in range(1, length):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
 
     return route
